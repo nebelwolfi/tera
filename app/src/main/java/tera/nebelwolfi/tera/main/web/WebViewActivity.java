7a871cc8de0780b276e608c25ada79e04cfdae44
Dennis Hasenstab <dr.backdooring@gmail.com>
2015-02-18 22:54:18 +0100

    reformat code

--- app/src/main/java/tera/nebelwolfi/tera/main/web/WebViewActivity.java
+++ app/src/main/java/tera/nebelwolfi/tera/main/web/WebViewActivity.java
@@ -17,7 +17,7 @@ public class WebViewActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.webview);
 
-        webview = (WebView)findViewById(R.id.webView1);
+        webview = (WebView) findViewById(R.id.webView1);
         webview.getSettings().setJavaScriptEnabled(true);
         // old idea: iframe of http://en.tera.gameforge.com/news/morenews/1
         // String customHtml = "<html><body><div style='border: 0px solid rgb(201, 0, 1); overflow: hidden; margin: 0px auto; max-width: 550px;'><iframe scrolling='no' src='http://en.tera.gameforge.com/news/morenews/1' style='border: 0px none; margin-left: 0px; height: 1600px; margin-top: -650px; width: 550px;'></iframe></div></body></html>";
